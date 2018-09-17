dc5225623939d228d410602e9ce16747601c9d12
Alexander Zapata <alexander.zapata@duke.edu>
2018-09-17 17:36:38 -0400

    Update aruco_tracker.py

--- aruco_tracker.py
+++ aruco_tracker.py
@@ -33,11 +33,24 @@ objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
 
+def inversePerspective(rvec, tvec): 
+    rvec, tvec = rvec.reshape((3, 1)), tvec.reshape((3, 1))
+    R, _ = cv2.Rodrigues(rvec)
+    R = np.matrix(R).T 
+    invTvec = np.dot(-R, np.matrix(tvec)) 
+    invRvec, _ = cv2.Rodrigues(R) 
+    invTvec = invTvec.reshape((1, 3)) 
+    invRvec = invRvec.reshape((1, 3)) 
+    return invRvec.tolist(), invTvec.tolist()
+
 def find_1D_midpoint(first_loc, second_loc):
     return (first_loc + second_loc) / 2
     
 def floor_midpoint(midpoint):
     return (math.floor(midpoint[0]), math.floor(midpoint[1]))
+
+def avg_of_vectors(vector_list):
+    return sum(vector_list)/float(len(vector_list))
 
 #Aruco image calibration
 images = glob.glob('calib_images/*.jpg')
@@ -77,6 +90,15 @@ while (True):
 
     if np.all(ids != None):
         rvec, tvec,_ = aruco.estimatePoseSingleMarkers(corners, 0.05, mtx, dist) #Estimate pose of each marker and return the values rvet and tvec---different from camera coefficients
+        inverse_rvec = []
+        inverse_tvec = []
+
+        for r, t in zip(rvec, tvec):
+            aruco.drawAxis(frame, mtx, dist, r, t, 0.1) #Draw Axis
+            i_rvec, i_tvec = inversePerspective(r[0], t[0])
+            inverse_rvec.append(i_rvec)
+            inverse_tvec.append(i_tvec)
+
         marker_midpoints = []
         for index, id in zip(range(len(ids)), ids):
             topleftX = math.floor(corners[index][0][0][0])
@@ -91,36 +113,40 @@ while (True):
             ###### DRAW ID #####
             cv2.putText(frame, "Id: " + str(id), floor_midpoint(midpoint), font, 1, (0,255,0),2,cv2.LINE_AA)
             marker_midpoints.append([math.floor(midpoint[0]), math.floor(midpoint[1])])
-
+            
         num_of_markers = len(marker_midpoints)
         visual_center_of_markers = (-1, -1)
-     
+        translational_vector = (-1, -1, -1)
+
         if num_of_markers == 0:
             pass
         elif num_of_markers == 1:
             visual_center_of_markers = floor_midpoint(marker_midpoints[0])
+            translational_vector = (inverse_tvec[0][0][0], inverse_tvec[0][0][1], inverse_tvec[0][0][2])
         elif num_of_markers == 2:
             visual_center_of_markers = floor_midpoint((find_1D_midpoint(marker_midpoints[0][0], marker_midpoints[1][0]), find_1D_midpoint(marker_midpoints[0][1], marker_midpoints[1][1])))
-        else: 
+            translational_vector = (find_1D_midpoint(inverse_tvec[0][0][0], inverse_tvec[1][0][0]), find_1D_midpoint(inverse_tvec[0][0][1], inverse_tvec[1][0][1]), find_1D_midpoint(inverse_tvec[0][0][2], inverse_tvec[1][0][2]))
+        else:
+            x_values = []
+            y_values = []
+            z_values = []
+            for index in range(len(inverse_tvec[0])):
+                x_values.append(inverse_tvec[index][0][0])
+                y_values.append(inverse_tvec[index][0][1])
+                z_values.append(inverse_tvec[index][0][2])
             visual_center_of_markers = floor_midpoint(polylabel([marker_midpoints]))
+            translational_vector = (avg_of_vectors(x_values), avg_of_vectors(y_values), avg_of_vectors(z_values))
  
         if visual_center_of_markers != (-1, -1):
             cv2.circle(frame, visual_center_of_markers, 30, (255, 0, 0)) 
-
-        #MORE DRAWING
-        for r, t in zip(rvec, tvec):
-            aruco.drawAxis(frame, mtx, dist, r, t, 0.1) #Draw Axis
-        	
+      
         aruco.drawDetectedMarkers(frame, corners) #Draw A square around the markers
 
+        s.send("{}/{}/{}".format(translational_vector[0], translational_vector[1], translational_vector[2]).encode())
     else:
-        #print("Markers not found") 
-        #print("midpoint X: n, Y: n")
-        s.send(b"/n/n/n")
+        s.send(b"n/n/n")
 
-
-
-        # Display the resulting frame
+    # Display the resulting frame
     cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
